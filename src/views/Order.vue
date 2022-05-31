2caa2ed02160804ef5a410512df22f89d8e94cb6
IsaacHdezA <aldoisaachernandez@outlook.com>
2022-05-31 02:07:28 -0500

    Changes to get on the go

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,8 +1,52 @@
 <template>
   <div>
+    <v-dialog
+      v-model="orderDialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card class="elevation-0">
+        <div class="pa-4">
+          <v-row align="center">
+            <v-col cols="1">
+              <v-btn
+                @click="orderDialog = false"
+                fab
+                elevation="0"
+                color="primary"
+              >
+                <v-icon color="scondary">fas fa-arrow-left</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col cols="10">
+              <v-row>
+                <v-col><h1>Mi pedido</h1></v-col>
+              </v-row>
+              <v-row>
+                <v-col><h3>16/Apr/2022</h3></v-col>
+              </v-row>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="1">
+              <v-chip class="pa-4" large text-color="secondary" color="primary">
+                amip ixula
+              </v-chip>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <OrderDetails />
+            </v-col>
+          </v-row>
+        </div>
+      </v-card>
+    </v-dialog>
+
     <v-row class="pt-6 px-6 row-title-home" align="center" justify="center">
       <v-col class="col-title-home">
-        <h1>Mis órdenes</h1>
+        <h1>Mis pedidos</h1>
       </v-col>
       <v-spacer></v-spacer>
       <v-col>
@@ -18,20 +62,27 @@
         ></v-text-field>
       </v-col>
     </v-row>
+
     <v-row>
       <div>
-        <v-card class="mx-8 pb-3" flat width="500" elevation="3">
+        <v-card
+          class="mx-8 pb-3"
+          flat
+          width="500"
+          elevation="3"
+          @click="orderDialog = true"
+        >
           <v-toolbar class="elevation-0 gradient-background">
-            <v-toolbar-text>
-              <v-badge></v-badge>
-            </v-toolbar-text>
+            <div>
+              <v-badge color="green"></v-badge>
+            </div>
             <v-spacer></v-spacer>
             <v-toolbar-title>Stickers Cool</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-toolbar-text>
+            <v-toolbar-title>
               <v-icon>far fa-calendar</v-icon>
               16 Apr 2021
-            </v-toolbar-text>
+            </v-toolbar-title>
           </v-toolbar>
 
           <v-row class="my-3 px-8">
@@ -53,13 +104,38 @@
 </template>
 
 <script>
+import { Order } from "/firebaseAPI/controllers/order.js";
+import OrderDetails from "../components/OrderDetails.vue";
+
 export default {
   name: "Order",
+
+  components: {
+    OrderDetails,
+  },
+
   data: () => {
     return {
-      data: [],
+      orderDialog: false,
+      allOrders: [],
     };
   },
+
+  computed: {},
+
+  async created() {
+    await this.readDocuments();
+  },
+
+  methods: {
+    async readDocuments() {
+      const O = new Order();
+      await O.readOrders().then(
+        (res) => (this.allOrders = O.docsObjectToArray(res))
+      );
+    },
+  },
+
   methods: {},
 };
 </script>

05da0a9beef79de54a92753e52586381b1fa56aa
MacHip3r <mac_258@hotmail.com>
2022-06-08 20:52:55 -0500

    cart button responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -116,47 +116,43 @@
       </v-card>
     </v-dialog>
 
-    <v-row
-      class="pt-6 px-6 row-title-home"
-      align="center"
-      justify="center"
-      fixed
-    >
+    <v-row class="pt-6 px-6 row-title-home" fixed>
       <v-col class="col-title-home" cols="7">
         <h1>Descubre</h1>
       </v-col>
-      <v-col cols="4">
-        <v-text-field
-          label="Buscar"
-          color="primary"
-          background-color="secondary"
-          append-icon="fas fa-search"
-          class="input-search"
-          filled
-          rounded
-          dense
-        ></v-text-field>
-      </v-col>
-      <v-col cols="1">
-        <v-badge
-          v-if="cart.o_products.length > 0"
-          color="red"
-          :content="cart.o_products.length"
-          overlap
-        >
-          <v-btn fab elevation="0" color="primary" @click="cartDialog = true">
+      <v-col>
+        <v-row>
+          <v-text-field
+            label="Buscar"
+            color="primary"
+            background-color="secondary"
+            append-icon="fas fa-search"
+            class="input-search"
+            filled
+            rounded
+            dense
+          ></v-text-field>
+          <v-badge
+            v-if="cart.o_products.length > 0"
+            color="red"
+            :content="cart.o_products.length"
+            overlap
+          >
+            <v-btn fab elevation="0" color="primary" @click="cartDialog = true">
+              <v-icon color="secondary">fas fa-shopping-cart</v-icon>
+            </v-btn>
+          </v-badge>
+          <v-btn
+            v-else
+            fab
+            elevation="0"
+            color="primary"
+            class="button-cart"
+            @click="cartDialog = true"
+          >
             <v-icon color="secondary">fas fa-shopping-cart</v-icon>
           </v-btn>
-        </v-badge>
-        <v-btn
-          v-else
-          fab
-          elevation="0"
-          color="primary"
-          @click="cartDialog = true"
-        >
-          <v-icon color="secondary">fas fa-shopping-cart</v-icon>
-        </v-btn>
+        </v-row>
       </v-col>
     </v-row>
 
